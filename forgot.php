fbe02517e792dd52d3b0a1ff07529807a9246a12
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2023-12-08 01:59:12 +0700

    Forgot Password

--- forgot.php
+++ forgot.php
@@ -1,4 +1,40 @@
+<?php
 
+require_once("config.php");
+
+date_default_timezone_set('Asia/Jakarta');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $name = $_POST['name'];
+    $newPassword = $_POST['password'];
+
+    if (!empty($email) && !empty($phone) && !empty($name) && !empty($newPassword)) {
+        $sql = "SELECT * FROM users WHERE email = :email AND number_phone = :phone AND name = :name";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':phone', $phone);
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+            $updateSql = "UPDATE users SET password = :password WHERE id = :userId";
+            $updateStmt = $db->prepare($updateSql);
+            $updateStmt->bindParam(':password', $hashedPassword);
+            $updateStmt->bindParam(':userId', $user['id']);
+            $updateStmt->execute();
+
+            $success = "Password successfully updated!";
+        } else {
+            $error = "Invalid credentials. Please check your details.";
+        }
+    } else {
+        $error = "Please fill in all the fields.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,19 +72,35 @@
                                 <p class="text-muted mb-4">Enter your email address.</p>
                             </div>
 
-                            <form action="api/forgot_new_password.php" method="POST">
-                                <div class="mb-3">
-                                    <label for="emailaddress" class="form-label">Email address</label>
-                                    <input class="form-control" type="text" name="email" id="emailaddress"
-                                        required="" placeholder="Enter email">
-                                </div>
-                                <div class="mb-1 mb-0 text-center">
-                                    <button class="btn btn-primary" type="submit" name="login" value="Submit"> Submit
-                                    </button>
-                                </div>
-
-                            </form>
-                        </div> <!-- end card-body -->
+                            <form method="POST">
+            <div class="mb-3">
+                <label for="emailaddress" class="form-label">Email address</label>
+                <input class="form-control" type="text" name="email" id="emailaddress" required="" placeholder="Enter email">
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone number</label>
+                <input class="form-control" type="text" name="phone" id="phone" required="" placeholder="Enter phone number">
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input class="form-control" type="text" name="name" id="name" required="" placeholder="Enter your name">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">New Password</label>
+                <input class="form-control" type="password" name="password" id="password" required="" placeholder="Enter new password">
+            </div>
+            <div class="mb-1 mb-0 text-center">
+                <button class="btn btn-primary" type="submit" name="login" value="Submit"> Submit </button>
+            </div>
+            <?php
+            if (isset($error)) {
+                echo '<div class="text-center text-danger mt-3">' . $error . '</div>';
+            } elseif (isset($success)) {
+                echo '<div class="text-center text-success mt-3">' . $success . '</div>';
+            }
+            ?>
+        </form>
+    </div> <!-- end card-body -->
                     </div>
                     <!-- end card -->
 
